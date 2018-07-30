cdacc3e414904765c482d333f3964591b85ef2d1
Victoria <victoria@MacBook-Pro.local>
2018-07-30 12:38:57 +0100

    added one more solution

--- makeAnagrams.py
+++ makeAnagrams.py
@@ -7,7 +7,7 @@ import re
 import sys
 
 # Complete the makeAnagram function below.
-def makeAnagram(a, b):
+def makeAnagram_1(a, b):
     result1 = 0
     result2 = 0
     s1 = set(a)
@@ -27,7 +27,29 @@ def makeAnagram(a, b):
     return result1 + result2
     
     
-    
+def makeAnagram(a, b):
+    letter_count_a = {}
+    for letter in a:
+        if letter in letter_count_a:
+            letter_count_a[letter] += 1
+        else:
+            letter_count_a[letter] = 1
+    print(letter_count_a)
+    letter_count_b = {}
+    for letter in b:
+        if letter in letter_count_b:
+            letter_count_b[letter] += 1
+        else:
+            letter_count_b[letter] = 1
+    print(letter_count_b)
+    res = 0
+    for key, val in letter_count_a.items():
+        res += abs(letter_count_b.get(key, 0 ) - val)
+    for key, val in letter_count_b.items():
+        if key not in letter_count_a:
+            res += val
+    return res
+
 
 if __name__ == '__main__':
 
