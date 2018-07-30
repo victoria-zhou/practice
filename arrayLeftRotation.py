56615e24d1bfa31c96420de326475e03b63b99dd
Victoria <victoria@macbook-pro.home>
2018-07-30 21:40:13 +0100

    added new solution - less run time

--- arrayLeftRotation.py
+++ arrayLeftRotation.py
@@ -7,24 +7,20 @@ import re
 import sys
 
 # Complete the rotLeft function below.
-def rotLeft(a, d):
-    for index in range(d):
-        temp = a[0]
-        for number in range(1, len(a)):
-            a[number-1]=a[number]
-        a[len(a)-1] = temp
-    print(a)
-    return a
+def rotLeft(arr, rot_times):
+    return arr[rot_times:] + arr[:rot_times]
+    # for index in range(d):
+    #     temp = a[0]
+    #     for number in range(1, len(a)):
+    #         a[number-1]=a[number]
+    #     a[len(a)-1] = temp
+    # return a
+        
 
+arr = [1,2,3,4,5]
+print arr
+print(rotLeft(arr,4))
 
-if __name__ == '__main__':
-
-    nd = input().split()
-
-    n = int(nd[0])
-
-    d = int(nd[1])
-
-    a = list(map(int, input().rstrip().split()))
-
-    result = rotLeft(a, d)
+arr = [1,2,3,4,5,6,8,9]
+print arr
+print(rotLeft(arr,5))
