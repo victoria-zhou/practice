59e60f2f4629812c971b60c23a515c27e1be5c84
Victoria <victoria@macbook-pro.home>
2018-08-09 21:46:50 +0100

    maxmim absolute value

---
+++ max_abs_value.py
@@ -0,0 +1,7 @@
+from math import *
+
+def computeDifference(a):
+        return max(a)-min(a) #know how to use max and min
+
+assert computeDifference([1,2,3,4,5]) == 4
+assert computeDifference([1,2,3,5,8,3,4,5]) == 7
