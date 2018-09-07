51d2fdd6b7271fbaa4e50d76cae9597b4795fd01
Victoria <victoria@macbook-pro.home>
2018-09-07 23:50:15 +0100

    count tripets

--- count_tripets.py
+++ count_tripets.py
@@ -8,13 +8,14 @@ from collections import defaultdict
 def count_triplets(arr, r):
     v2 = defaultdict(int)
     v3 = defaultdict(int)
-    print(v2)
-    print(v3)
     count = 0
     for k in arr:
         count += v3[k]
         v3[k*r] += v2[k]
         v2[k*r] += 1
+
+        print(v3[k*r])
+        print(v2[k*r])
 
     return count
 
