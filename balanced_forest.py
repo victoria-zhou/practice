11f2795408321fe202f6e073621858a526dc283f
Victoria <victoria@mac-c82a14118447.home>
2018-09-23 23:51:30 +0100

    balanced forest

---
+++ balanced_forest.py
@@ -0,0 +1,7 @@
+Greg has a tree of nodes containing integer data. He wants to insert a node with some non-zero integer value 
+somewhere into the tree. His goal is to be able to cut two edges and have the values of each of the three new 
+trees sum to the same amount. This is called a balanced forest. Being frugal, the data value he inserts should 
+be minimal. Determine the minimal amount that a new node can have to allow creation of a balanced forest. 
+If it's not possible to create a balanced forest, return -1.
+
+Solution to be updated
